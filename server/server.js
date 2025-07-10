f475973de842c98bc8ee1de7f906340438c37f0c
code-with-neeraj <neerajkr145518@gmail.com>
2025-07-10 07:36:24 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express  from "express";
+import cors from "cors";
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
+
+import connectDB from "./config/mongodb.js";
+import authRouter from './routes/authRoutes.js'
+import userRouter from "./routes/userRoutes.js";
+
+
+
+// 3:42:42
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+connectDB();
+
+const allowedOrigins = ['http://localhost:5173']
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({origin: allowedOrigins ,credentials: true}));
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server started on PORT: ${PORT}`);
+});
